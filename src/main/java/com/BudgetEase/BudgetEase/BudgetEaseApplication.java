60fa4b3766aea2355752da1dbe8a9fa7bfb7fca1
Prabhat Deshmukh <prabhat.deshmukh22@gmail.com>
2025-03-23 16:12:43 +0530

    first commit

---
+++ src/main/java/com/BudgetEase/BudgetEase/BudgetEaseApplication.java
@@ -0,0 +1,13 @@
+package com.BudgetEase.BudgetEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetEaseApplication.class, args);
+	}
+
+}
